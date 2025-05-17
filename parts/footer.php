7233a6e099db14b4539b272c720e8ce14ce6233d
bucel-sebastian <bucel.ionsebastian@gmail.com>
2025-05-17 22:54:39 +0300

    [Feature] Added GA

--- parts/footer.php
+++ parts/footer.php
@@ -53,3 +53,9 @@
         </div>
     </div>
 </footer>
+
+
+<!-- Google Tag Manager (noscript) -->
+<noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-K948JQW4"
+height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+<!-- End Google Tag Manager (noscript) -->
--- parts/head.php
+++ parts/head.php
@@ -7,3 +7,11 @@
 <link rel="icon" type="image/png" sizes="16x16" href="<?php echo MAGURA_2025_THEME_URL . '/assets/img/favicon/favicon-16x16.png' ?>">
 <link rel="icon" type="image/png" sizes="32x32" href="<?php echo MAGURA_2025_THEME_URL . '/assets/img/favicon/favicon-32x32.png' ?>">
 <link rel="shortcut icon" href="<?php echo MAGURA_2025_THEME_URL . '/assets/img/favicon/favicon.ico'; ?>">
+
+<!-- Google Tag Manager -->
+<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-K948JQW4');</script>
+<!-- End Google Tag Manager -->
