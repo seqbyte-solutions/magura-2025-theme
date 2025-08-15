fd8e940b0f8d78060a2fd47da3b5dbe1eba991ff
bucel-sebastian <bucel.ionsebastian@gmail.com>
2025-08-16 00:30:41 +0300

    end

--- templates/page-entry.php
+++ templates/page-entry.php
@@ -1,19 +1,19 @@
 <?php
-wp_enqueue_script('campaign-form', MAGURA_2025_THEME_URL . '/assets/js/campaign-form.js');
-wp_localize_script(
-    'campaign-form',
-    'campaignData',
-    [
-        'api_url' => "https://api-magura.promoapp.ro/api/v1/",
-        'api_token' => "a1b2c3d4e5f6g7h8i9j0k1l2m3n4o5p6q7r8s9t0",
-        'transition_img' => MAGURA_2025_THEME_URL . '/assets/img/campaign-transition.svg',
-        'prize_img_rucsac' => MAGURA_2025_THEME_URL . '/assets/img/prize_ghiozdan.png',
-        'prize_img_vacanta' => MAGURA_2025_THEME_URL . '/assets/img/prize_vacanta.png',
-        'prize_img_cani' => MAGURA_2025_THEME_URL . '/assets/img/prize_cani.png',
-        'ajax_url' => admin_url('admin-ajax.php'),
-        'security' => wp_create_nonce('send_email_nonce'),
-    ]
-);
+// wp_enqueue_script('campaign-form', MAGURA_2025_THEME_URL . '/assets/js/campaign-form.js');
+// wp_localize_script(
+//     'campaign-form',
+//     'campaignData',
+//     [
+//         'api_url' => "https://api-magura.promoapp.ro/api/v1/",
+//         'api_token' => "a1b2c3d4e5f6g7h8i9j0k1l2m3n4o5p6q7r8s9t0",
+//         'transition_img' => MAGURA_2025_THEME_URL . '/assets/img/campaign-transition.svg',
+//         'prize_img_rucsac' => MAGURA_2025_THEME_URL . '/assets/img/prize_ghiozdan.png',
+//         'prize_img_vacanta' => MAGURA_2025_THEME_URL . '/assets/img/prize_vacanta.png',
+//         'prize_img_cani' => MAGURA_2025_THEME_URL . '/assets/img/prize_cani.png',
+//         'ajax_url' => admin_url('admin-ajax.php'),
+//         'security' => wp_create_nonce('send_email_nonce'),
+//     ]
+// );
 // wp_enqueue_style('campaign-form', MAGURA_2025_THEME_URL . '/assets/js/campaign-form.css');
 ?>
 
@@ -23,7 +23,7 @@ wp_localize_script(
             <h2>Înscrie-te</h2>
         </div>
         <div class="campaign-form-container">
-            <div id="campaign-form-app"></div>
-        </div>
+            <h4>Campania s-a încheiat pe 15 august 2025</h4>
+                </div>
     </div>
 </section>
